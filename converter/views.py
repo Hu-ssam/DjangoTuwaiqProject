524dc685aaf0134afc7b528366e60b44235359c1
Hu-ssam <hussam.moh02@gmail.com>
2025-09-05 03:34:08 +0300

    make it better

--- converter/views.py
+++ converter/views.py
@@ -64,4 +64,4 @@ def convert_currency(request):
             }
     if request.user.id is None:
          context['history'] = None
-    return render(request,'/Users/hussamalasmari/Desktop/Django Course/deploymentprojHussam/converter/templates/index.html',context)
+    return render(request,'templates/index.html',context)
